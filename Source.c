71484878ec396a3e83e1676e84b3bbf4f9823328
smc9759 <42694805+smc9759@users.noreply.github.com>
2022-10-31 10:01:00 +0900

    Add files via upload
    반복문에 사용되는 i의 최대값과, 중괄호를 사용하지 않을 경우 실행 경과

---
+++ Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for ( i = 0; i < 100; i++)
+    printf("for 반복문\n");        // 이 줄만 반복됨
+    printf("Hello, world!\n");    // 이 줄은 한 번만 실행됨
+    printf("%d", i);               // now 100
+    return 0;
+}
