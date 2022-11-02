9f24f9d29de34ce49787e98c662ee8a0ad5f330e
smc9759 <42694805+smc9759@users.noreply.github.com>
2022-11-02 14:35:33 +0900

    Add files via upload
    컴파일은 되는데 실행이 안된다
    Exception thrown at 0x00007FFF3EA92487 (ucrtbased.dll) in Project19.exe: 0xC0000005: Access violation reading location 0x0000000000000024.
    주소를 보고 찾아갔는데 건물이 없다

--- Source.c
+++ Source.c
@@ -1,35 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void main()
 {
-	int size;
-	scanf("%d", &size);
-	int** m = malloc(sizeof(int*) * size);
+	char s1 = "Hello";
 
-	for (int col = 0; col < size-1; col++)
-	{
-		m[col] = malloc(sizeof(int) * size);
-	}
-
-	for (int i = 0; i < size-1; i++)
-	{
-		for (int j = 0; j < size-1; j++)
-		{
-			if (i = j) m[i][j] = 1;
-			else m[i][j] = '0';
-		}
-	}
-	//print
-	for (int i = 0; i < size-1; i++)
-	{
-		for (int j = 0; j < size-1; j++)
-		{
-			printf("%d ", m[i][j]);
-		}
-		printf("\n");
-	}
+	printf("%s", s1);
 
 	return 0;
 }
