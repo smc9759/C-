efb7c15b3e723dc38af1241049f3e29424b2d96f
smc9759 <42694805+smc9759@users.noreply.github.com>
2022-11-02 13:54:48 +0900

    Add files via upload
    포인터에 할당된 메모리를 3차원 배열처럼 사용하기

--- Source.c
+++ Source.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+#include <stdlib.h>
 
 int main()
 {
-    int* numPtr = malloc(sizeof(int) * 10);    // int 10개 크기만큼 동적 메모리 할당
-    // *numPtr = { &int1, &int2, &int3, &int4, &int5, &int6 }
-    numPtr[0] = 10;    // 배열처럼 인덱스로 접근하여 값 할당
-    numPtr[9] = 20;    // 배열처럼 인덱스로 접근하여 값 할당
+	long long*** m = malloc(sizeof(long long**) * 2);
 
-    printf("%d\n", numPtr[0]);    // 배열처럼 인덱스로 접근하여 값 출력
-    printf("%d\n", numPtr[9]);    // 배열처럼 인덱스로 접근하여 값 출력
+	for (int depth = 0; depth < 2; depth++)
+	{
+		m[depth] = malloc(sizeof(long long*) * 3);
+		for (int row = 0; row < 3; row++)
+		{
+			m[depth][row] = malloc(sizeof(long long) * 5);
+		}
+	}
+	//아래는 컴파일 에러남. **m[i] 에는 접근할 수 없는듯. 
+	// m[i][j]는 가능하다 높이, 세로, 가로 순서이므로 높이, 세로만 설정한 것은 아직 가로가 없는 것이다
+	/*
+	for (int i =0; i<2; i++)
+	{
+		**m[i]=malloc(sizeof(long long *)*3);
+	}
+	for ( int i = 0; i<3; i++)
+	{
+		*m[i]=malloc(sizeof(long long)*5);
+	}
+*/
+	m[1][2][4] = 100;
 
-    free(numPtr);    // 동적으로 할당한 메모리 해제
+	printf("%lld\n", m[1][2][4]);
 
-    return 0;
+	for (int depth = 0; depth < 2; depth++)
+	{
+		for (int row = 0; row < 3; row++)
+		{
+			free(m[depth][row]);
+		}
+		free(m[depth]);
+	}
+	/*
+		for ( int i = 0; i<5; i++)
+		{
+			free(m[i]);
+		}
+		for ( int i = 0; i<3; i++)
+		{
+			free(*m[i]);
+		}
+		for (int i =0; i<2; i++)
+		{
+			free(**m[i]);
+		}
+	*/
+	return 0;
 }
