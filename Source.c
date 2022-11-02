d48ba785398e9a37c3a63f6c841e7fd03af33c68
smc9759 <42694805+smc9759@users.noreply.github.com>
2022-11-02 14:21:10 +0900

    Add files via upload
    심사문제 단위행렬 만들기 하다가 만거
    할당이 제대로 안되었는지 쓰레기값이 나온다
    size 도 -1해서 잘못된 인덱스에 접근하지는 않은것 같은데

--- Source.c
+++ Source.c
@@ -1,55 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	long long*** m = malloc(sizeof(long long**) * 2);
+	int size;
+	scanf("%d", &size);
+	int** m = malloc(sizeof(int*) * size);
 
-	for (int depth = 0; depth < 2; depth++)
+	for (int col = 0; col < size-1; col++)
 	{
-		m[depth] = malloc(sizeof(long long*) * 3);
-		for (int row = 0; row < 3; row++)
-		{
-			m[depth][row] = malloc(sizeof(long long) * 5);
-		}
+		m[col] = malloc(sizeof(int) * size);
 	}
-	//아래는 컴파일 에러남. **m[i] 에는 접근할 수 없는듯. 
-	// m[i][j]는 가능하다 높이, 세로, 가로 순서이므로 높이, 세로만 설정한 것은 아직 가로가 없는 것이다
-	/*
-	for (int i =0; i<2; i++)
-	{
-		**m[i]=malloc(sizeof(long long *)*3);
-	}
-	for ( int i = 0; i<3; i++)
-	{
-		*m[i]=malloc(sizeof(long long)*5);
-	}
-*/
-	m[1][2][4] = 100;
 
-	printf("%lld\n", m[1][2][4]);
-
-	for (int depth = 0; depth < 2; depth++)
+	for (int i = 0; i < size-1; i++)
 	{
-		for (int row = 0; row < 3; row++)
+		for (int j = 0; j < size-1; j++)
 		{
-			free(m[depth][row]);
+			if (i = j) m[i][j] = 1;
+			else m[i][j] = '0';
 		}
-		free(m[depth]);
 	}
-	/*
-		for ( int i = 0; i<5; i++)
+	//print
+	for (int i = 0; i < size-1; i++)
+	{
+		for (int j = 0; j < size-1; j++)
 		{
-			free(m[i]);
+			printf("%d ", m[i][j]);
 		}
-		for ( int i = 0; i<3; i++)
-		{
-			free(*m[i]);
-		}
-		for (int i =0; i<2; i++)
-		{
-			free(**m[i]);
-		}
-	*/
+		printf("\n");
+	}
+
 	return 0;
 }
