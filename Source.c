ea538945197dc9255a3302db25e82cccf234ae13
smc9759 <42694805+smc9759@users.noreply.github.com>
2022-11-07 11:31:20 +0900

    Add files via upload
    환타

--- Source.c
+++ Source.c
@@ -1,16 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS    // scanf 보안 경고로 인한 컴파일 에러 방지
 #include <stdio.h>
 
-int main()
-{
-    char s1[10];    // 크기가 10인 char형 배열을 선언
-    char s2[10];    // 크기가 10인 char형 배열을 선언
+int main(void) {
 
-    printf("문자열을 두 개 입력하세요: ");
-    scanf("%s %s", s1, s2);    // 표준 입력에서 공백으로 구분된 문자열 두 개를 입력받음
-
-    printf("%s\n", s1);    // s1의 내용을 출력
-    printf("%s\n", s2);    // s2의 내용을 출력
+    int num1 = 2;
+    switch (num1)
+    {
+    case 1:
+        printf("콜라를 내보냄");
+        break;
+    case 2:
+    {
+        int num2 = 5500;
+        printf("%d\n", num2);
+    }
+    break;
+    case 3:
+        printf("환타 혹은 닥터 페퍼를 내보냄");
+        break;
+    default:
+        printf("없는 메뉴");
+        break;
+    }
 
     return 0;
 }
